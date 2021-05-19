ad65cd67fdfc8fb0ad6f0da6468d1ad7c4f598dc
Yassin <saied.yassin@gmail.com>
2021-05-20 01:40:54 +0200

    webpack js

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,24 @@
 /*
  * Welcome to your app's main JavaScript file!
  *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
+ * We recommend including the built version of this JavaScript file*/
+   var $ = require('./js/jquery');
+          require('./js/popper');   
+          require('./js/bootstrap');   
+          require('./js/');   
+          require('./js/bootstrap');  
+ 
+ /* (and its CSS file) in your base layout (base.html.twig).
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/base.css';
+import './styles/bootstrap.min.css';
+import './styles/bootstrap-datepicker.css';
+import './styles/bootstrap-slider.min.css';
+import './styles/bouk.css';
+import './styles/explorer_fas.css';
+import './styles/form.css';
+import './styles/list_ads.css';
+import './styles/show.css';
 
-// start the Stimulus application
-import './bootstrap';
